eac8dc233c9e22389b89206c32ed71c11fc4e4d1
Leonardo Prado <leonardo.rprado@gmail.com>
2022-10-11 13:42:58 +0100

    finalizado req. 06

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Questions from '../components/Questions';
 
 class Game extends Component {
+  state = {
+    questions: [],
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    this.fetchQuestions();
+  }
+
+  fetchQuestions = async () => {
+    const numQuestions = 5;
+    const invalidToken = 3;
+    const token = localStorage.getItem('token');
+    const END_POINT = `https://opentdb.com/api.php?amount=${numQuestions}&token=${token}`;
+    const response = await fetch(END_POINT);
+    const data = await response.json();
+    const { response_code: responseCode, results: questions } = data;
+
+    if (responseCode === invalidToken) {
+      const { history } = this.props;
+      localStorage.removeItem('token');
+      history.push('/');
+    }
+
+    this.setState({ questions, isLoading: false });
+  };
+
   render() {
+    const { questions, isLoading } = this.state;
     return (
-      <>
+      <div>
         <Header />
-        <div>Game</div>
-      </>
+        {
+          isLoading
+            ? <p>Loading...</p>
+            : <Questions questions={ questions } />
+        }
+      </div>
     );
   }
 }
 
-export default Game;
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Game);
