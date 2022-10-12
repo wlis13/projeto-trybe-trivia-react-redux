050cf051619434c609714edc5f32a7e11cf85481
Matheus Emanuel <matheusleitecordeiro@gmail.com>
2022-10-12 17:43:53 -0300

    req 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import RedirectButton from '../components/RedirectButton';
 
 class Ranking extends Component {
   render() {
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <>
+        <div data-testid="ranking-title">Ranking</div>
+        <RedirectButton dataTestId="btn-go-home" nameBtn="Home" path="/" />
+      </>
     );
   }
 }
